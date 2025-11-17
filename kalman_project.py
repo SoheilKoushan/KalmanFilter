71cf752009d68f8aac9731cd15a5c4b211a5db4b
aaron <chesscraft@outlook.com>
2025-11-17 18:49:50 +0800

    Added internal documentation

--- kalman_project.py
+++ kalman_project.py
@@ -2,3 +2,11 @@
 
 import pandas as pd
 import numpy as np
+
+'''
+1. We must simulate a ground truth 2D trajectory and noiosy position measurements
+2. Derive and implement a discrete kalman filter for a constant velocity
+3. Evaluate accuracy versus a baseline (like a moving average using RMSE)
+4. Validate statistical assumptions using innovation or residual tests
+5. Stretch to handle missing measurements and occasional outliers robustly
+'''
