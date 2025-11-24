adc098305abb3b4c435fd6d71093b6cfd18fe1a5
aaron <chesscraft@outlook.com>
2025-11-24 18:52:57 +0800

    Improved comment formatting

--- kalman_project.py
+++ kalman_project.py
@@ -38,7 +38,8 @@ def simulate_truth(cfg: SimConfig):
     x = 0.5 * t**2 + 10*np.sin(0.3*t)
     y = 0.3 * t**2 + 8*np.cos(0.27*t + 0.8)
 
-    vx = np.gradient(x, cfg.dt) # take the numerical derivative of x wrt time, where time steps are evenly spaced by dt.
+    # take the numerical derivative of x wrt time, where time steps are evenly spaced by dt.
+    vx = np.gradient(x, cfg.dt) 
     vy = np.gradient(y, cfg.dt)
 
     X = np.vstack([x, vx, y, vy]).T # shape (n, 4)
